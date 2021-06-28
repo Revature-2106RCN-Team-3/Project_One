d21eadf948e08c6a1d65df51a22b5fc0e802d236
Kent Smith <kentrsmith1996@gmail.com>
2021-06-28 18:30:51 -0400

    Added https redirect middleware, and temporary dev ssl certificate and key. Modified the server entry point to run both an http and https server. Project currently does not run so I can't test this, not my code breaking.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import './pre-start'; // Must be the first import
+import http from "http";
+import https from "https";
+import fs from "fs";
 import app from '@server';
 import logger from '@shared/Logger';
 
 
 // Start the server
-const port = Number(process.env.PORT || 3000);
-app.listen(port, () => {
-    logger.info('Express server started on port: ' + port);
+const httpPort = Number(process.env.HTTPPORT || 3000);
+const httpsPort = Number(process.env.HTTPSPORT || 3001);
+http.createServer(app).listen(httpPort, () => {
+    
+    logger.info('Http server started on port: ' + httpPort);
+
+});
+https.createServer({
+
+    key: fs.readFileSync("server.key"),
+    cert: fs.readFileSync("server.cert")
+
+}, app).listen(httpsPort, () => {
+
+    logger.info('Https server started on port: ' + httpsPort);
+
 });
